b6263ea5088396b63815e391ec6dac14f4ea0cab
Teilnehmer <Teilnehmer@BBQTEIL-CPBCFQB.Bildung.zz>
2016-12-07 11:24:52 +0100

    KlassenraumUpdate funktioniert1

--- view/klassenraumShowUpdate.php
+++ view/klassenraumShowUpdate.php
@@ -28,7 +28,6 @@
                                                                        ?>
                                 </select>
                             </td>
-                            <input type='hidden' name='id' value='<?php echo $objects['klassenraum']->getId(); ?>'/>
                             
                         </tr>
 
